738155444a072f9654c3c1727385347424fa7800
lukasrides <89727604+lukasrides@users.noreply.github.com>
2025-01-06 16:39:03 +0100

    Implement price history loading

--- src/cryptoHistoryHandler.py
+++ src/cryptoHistoryHandler.py
@@ -1,33 +1,53 @@
 import ccxt
+import os 
 import pandas as pd
-import numpy as np
+import warnings
 from datetime import datetime, timezone, timedelta
 
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 class cryptoHistoryHandler:
     def __init__(self,symbol,timeframe,lookback):
         # Initialize exchange as Binance
         self.exchange = ccxt.binance()
         self.symbol = symbol
-        self.timeframe = timeframe  # timeframe to inspect 
+        self.timeframe = timeframe  # timeframe to inspect - 1d, 1h, 1m
         self.lookback = lookback    # lookback in years
-        self.savepath = f'src\history\{self.symbol}-history.csv'
+        self.symbolReplaced = self.symbol.replace('/','-')
+        self.filename = f'{self.symbolReplaced}-{self.timeframe}-{self.lookback}-history.csv'
+        self.savepath = os.path.join('src','history',self.filename)
 
     # method to load and store historic data of a specific currency
     def updateHistory(self):
         # 5 years is 0 to 4
-        history = pd.DataFrame()
-        tempHistory = np.array()
+        # Define column names
+        columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
+        history = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
         for years in range(self.lookback):
-            startDate = datetime.now(tz=timezone.utc).strftime('%Y-%m-%dT00:00:00Z') - timedelta(days=365)
+            startDate = datetime.now(tz=timezone.utc) - timedelta(days=365*(years+1))
+            startDate = startDate.strftime('%Y-%m-%d %H:%M:%S')
             since = self.exchange.parse8601(startDate) 
-            # Fetch historical data
-            data = self.exchange.fetch_ohlcv(self.symbol, self.timeframe, since)
-            # TODO: Store data
-    
+            data = self.exchange.fetch_ohlcv(self.symbol, self.timeframe, since) # Fetch historical data
+            dataFrame = pd.DataFrame(data, columns=columns) # put loaded data into DataFrame
+            # Convert timestamps from ms to y-m-d H:M:S format
+            dataFrame['timestamp'] = dataFrame['timestamp'].apply(
+                lambda time: datetime.fromtimestamp(time / 1000).strftime('%Y-%m-%d')
+            )
+            history = pd.concat([history,dataFrame],ignore_index=True)
+        history['timestamp'] = pd.to_datetime(history['timestamp'],format='%Y-%m-%d')
+        history.sort_values(by='timestamp',inplace=True,ascending=True) 
+        history.to_csv(self.savepath) # save to csv
+
+    def getHistory(self):
+        history = pd.read_csv(self.savepath,index_col=0)
+        history['timestamp'] = pd.to_datetime(history['timestamp'],format='%Y-%m-%d')
+        history = history.set_index('timestamp')
+        print('Frequency: ',history[timedelta].dt.freq)
+        return history
+
     def setSymbol(self,symbol):
         self.symbol = symbol
-        self.savepath = f'src\history\{self.symbol}-history.csv'
+        self.savepath = f'src\history\{self.symbol}-{self.timeframe}-history.csv'
     
     def setTimeframe(self,timeframe):
         self.timeframe = timeframe
@@ -36,16 +56,3 @@ class cryptoHistoryHandler:
         self.lookback = lookback
 
 
-
-
-# Fetch historical OHLCV (Open, High, Low, Close, Volume)
-symbol = 'BTC/USDT'  # BTC to USDT
-timeframe = '1d'  # Daily data
-since = exchange.parse8601('2024-01-01T00:00:00Z')  # Start date
-
-# Fetch historical data
-data = exchange.fetch_ohlcv(symbol, timeframe, since)
-
-# Print data (timestamp, open, high, low, close, volume)
-for row in data:
-    print(datetime.fromtimestamp(float(row[0])/1000,tz=timezone.utc).strftime('%Y-%m-%d'),row[1:])
--- src/stockForecaster.py
+++ src/stockForecaster.py
@@ -1,9 +1,35 @@
 from nixtla import NixtlaClient
+from dotenv import load_dotenv
+import os
+import matplotlib.pyplot as plt
+import plotly.express as px
+import cryptoHistoryHandler as chh
 
-
-
+# lodas env variables
+load_dotenv()
 # Initialize nixtla with my own API-Key TODO: Change to env file
 nixtla_client = NixtlaClient(
-    api_key = 'my_api_key_provided_by_nixtla'
+    api_key = os.getenv('NIXTLA_API_KEY'),
 )
+
+# Initialize cryptoHistoryHandler (symbol,timeframe,lookback)
+cryptoHandler = chh.cryptoHistoryHandler('BTC/USDT','1d',5)
+#cryptoHandler.updateHistory() # Update history
+history = cryptoHandler.getHistory()
+
+"""forecast = nixtla_client.forecast(history,
+                                  h=30,
+                                  level=[50,80,90],
+                                  time_col='timestamp',
+                                  target_col='close',
+                                  freq='D'
+                                  )
+
+chart = nixtla_client.plot(history, forecast,
+                           time_col='timestamp',
+                           target_col='close',
+                           engine='plotly') # Plot closing prices
+chart.update_layout(title=f'{cryptoHandler.symbol} Closing Prices')
+chart.show()
+"""
 
