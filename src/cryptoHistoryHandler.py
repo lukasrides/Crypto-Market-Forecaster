61ffdad4b918bc12bd3921288fe95cd38c53f83a
Lukas N <lukas.nichelmann@web.de>
2025-01-06 23:23:54 +0100

    Implement CLI user guidance

--- src/cryptoHistoryHandler.py
+++ src/cryptoHistoryHandler.py
@@ -3,18 +3,19 @@ import os
 import pandas as pd
 import warnings
 from datetime import datetime, timezone, timedelta
+from tqdm import tqdm
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 class cryptoHistoryHandler:
-    def __init__(self,symbol,timeframe,lookback):
+    def __init__(self,symbol,timeframe):
         # Initialize exchange as Binance
         self.exchange = ccxt.binance()
         self.symbol = symbol
         self.timeframe = timeframe  # timeframe to inspect - 1d, 1h, 1m
-        self.lookback = lookback    # lookback in years
+        self.lookback = 5    # lookback in years
         self.symbolReplaced = self.symbol.replace('/','-')
-        self.filename = f'{self.symbolReplaced}-{self.timeframe}-{self.lookback}-history.csv'
+        self.filename = f'{self.symbolReplaced}-{self.timeframe}-history.csv'
         self.savepath = os.path.join('src','history',self.filename)
 
     # method to load and store historic data of a specific currency
@@ -23,37 +24,58 @@ class cryptoHistoryHandler:
         # Define column names
         columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
         history = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-        for years in range(self.lookback):
+        for years in tqdm(range(self.lookback),desc='Loading historical data'):
             startDate = datetime.now(tz=timezone.utc) - timedelta(days=365*(years+1))
             startDate = startDate.strftime('%Y-%m-%d %H:%M:%S')
             since = self.exchange.parse8601(startDate) 
             data = self.exchange.fetch_ohlcv(self.symbol, self.timeframe, since) # Fetch historical data
             dataFrame = pd.DataFrame(data, columns=columns) # put loaded data into DataFrame
-            # Convert timestamps from ms to y-m-d H:M:S format
-            dataFrame['timestamp'] = dataFrame['timestamp'].apply(
-                lambda time: datetime.fromtimestamp(time / 1000).strftime('%Y-%m-%d')
-            )
+            if self.timeframe == '1d':
+                # Convert timestamps from ms to y-m-d H:M:S format
+                dataFrame['timestamp'] = dataFrame['timestamp'].apply(
+                    lambda time: datetime.fromtimestamp(time / 1000).strftime('%Y-%m-%d')
+                )
+                history['timestamp'] = pd.to_datetime(history['timestamp'],format='%Y-%m-%d')
+            # TODO: Implement 1h and 1m timeframes
+            """elif self.timeframe == '1h':
+                # Convert timestamps from ms to y-m-d H:M:S format
+                dataFrame['timestamp'] = dataFrame['timestamp'].apply(
+                    lambda time: datetime.fromtimestamp(time / 1000).strftime('%Y-%m-%d %H')
+                )
+                history['timestamp'] = pd.to_datetime(history['timestamp'],format='%Y-%m-%d %H')
+            elif self.timeframe == '1m':
+                # Convert timestamps from ms to y-m-d H:M:S format
+                dataFrame['timestamp'] = dataFrame['timestamp'].apply(
+                    lambda time: datetime.fromtimestamp(time / 1000).strftime('%Y-%m-%d %H:%M')
+                )
+                history['timestamp'] = pd.to_datetime(history['timestamp'],format='%Y-%m-%d %H:%M')"""
             history = pd.concat([history,dataFrame],ignore_index=True)
-        history['timestamp'] = pd.to_datetime(history['timestamp'],format='%Y-%m-%d')
+        
+        history['timestamp'] = pd.to_datetime(history['timestamp'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
         history.sort_values(by='timestamp',inplace=True,ascending=True) 
         history = history[~history['timestamp'].duplicated(keep='first')]
+        history = history.dropna(subset=['timestamp'])  # Remove entries without timestamp
         history.to_csv(self.savepath) # save to csv
 
     # method to get the historic data of a specific symbol
     def getHistory(self):
-        return pd.read_csv(self.savepath, index_col=0)
+        history = pd.read_csv(self.savepath, index_col=0)
+        history['timestamp'] = pd.to_datetime(history['timestamp'], format='%Y-%m-%d', errors='coerce')
+        return history
 
     # method to set the currently selected symbol
     def setSymbol(self,symbol):
         self.symbol = symbol
-        self.savepath = f'src\history\{self.symbol}-{self.timeframe}-history.csv'
-    
+        self.symbolReplaced = self.symbol.replace('/','-')
+        self.filename = f'{self.symbolReplaced}-{self.timeframe}-history.csv'
+        self.savepath = os.path.join('src','history',self.filename)
+
+    # TODO: Implement 1h and 1m timeframes
     # method to set the currently selected timeframe
     def setTimeframe(self,timeframe):
         self.timeframe = timeframe
-    
-    # method to set the currently selected lookback
-    def setLookback(self,lookback):
-        self.lookback = lookback
+        self.filename = f'{self.symbolReplaced}-{self.timeframe}-history.csv'
+        self.savepath = os.path.join('src','history',self.filename)
+
 
 
--- src/stockForecaster.py
+++ src/stockForecaster.py
@@ -1,10 +1,62 @@
 from nixtla import NixtlaClient
+import logging
 from dotenv import load_dotenv
+import warnings
 import os
-#import plotly.express as px
 import cryptoHistoryHandler as chh
-#from datetime import datetime, timezone,timedelta
-import pandas as pd
+import inquirer
+
+# Set logging level to ERROR to suppress info and debug messages
+logging.getLogger('nixtla').setLevel(logging.ERROR)
+# Suppress specific warnings
+warnings.filterwarnings("ignore", message="`df` contains the following exogenous features")
+
+def setParameters():
+    choices = [
+    inquirer.List(
+        'action',
+        message='Please select the desired symbol.',
+        choices=['BTC/USDT','ETH/USDT','XRP/USDT','IOTA/USDT'],
+    ),
+    ]
+    selection = inquirer.prompt(choices)
+    cryptoHandler.setSymbol(selection['action'])
+
+    # TODO: Add timeframe selection
+    """# set timeframe
+    choices = [
+    inquirer.List(
+        'action',
+        message='Please select the desired timeframe.',
+        choices=['1h','1d','1m'],
+    ),
+    ]
+    selection = inquirer.prompt(choices)
+    cryptoHandler.setTimeframe(selection['action'])"""
+
+def forecast():
+    print('\nForecasting... 📉📈')
+    history = cryptoHandler.getHistory()
+    forecast = nixtla_client.forecast(history,
+                                h=14,
+                                level=[50,80,90],
+                                time_col='timestamp',
+                                target_col='close',
+                                freq='D'
+                                )
+
+    chart = nixtla_client.plot(history, forecast,
+                            level=[50,80,90],
+                            time_col='timestamp',
+                            target_col='close',
+                            max_insample_length=365,
+                            engine='plotly') # Plot closing prices
+
+    chart.update_layout(title=f'{cryptoHandler.symbol} Closing Prices', width=2400, height=1200)
+    chart.show()
+    print('\nPlot opened in a browser window. 🚀🌕\n')
+
+
 
 # lodas env variables
 load_dotenv()
@@ -13,29 +65,37 @@ nixtla_client = NixtlaClient(
 )
 
 # Initialize cryptoHistoryHandler (symbol,timeframe,lookback)
-cryptoHandler = chh.cryptoHistoryHandler('BTC/USDT','1d',5)
-#cryptoHandler.updateHistory() # Update history
-history = cryptoHandler.getHistory()
+cryptoHandler = chh.cryptoHistoryHandler('BTC/USDT','1d')
+quit = False
 
-# TODO: Add user Interface
+while not quit:
+    print(f'\n₿₿ Welcome to the Crypto Forecaster! ₿₿ \n\n Currently selected symbol: {cryptoHandler.symbol}\n Currently selected timeframe: {cryptoHandler.timeframe}\n ')
+    choices = [
+        inquirer.List(
+            'action',
+            message='What would you like to do?',
+            choices=['Set Parameters','Update History','Forecast','Quit'],
+        ),
+    ]
+    selection = inquirer.prompt(choices)
 
-# TODO: Create function
-forecast = nixtla_client.forecast(history,
-                                  h=14,
-                                  level=[50,80,90],
-                                  time_col='timestamp',
-                                  target_col='close',
-                                  freq='D'
-                                  )
+    match selection['action']:
+        case 'Set Parameters':
+            setParameters() # Set parameters
+        case 'Update History':
+            cryptoHandler.updateHistory() # Update history
+        case 'Forecast':
+            if not os.path.exists(cryptoHandler.savepath):
+                print('No history available. Starting update...')
+                cryptoHandler.updateHistory()
+            forecast()
+        case 'Quit':
+            quit = True
+            print('Goodbye!')
 
-chart = nixtla_client.plot(history, forecast,
-                           level=[50,80,90],
-                           time_col='timestamp',
-                           target_col='close',
-                           max_insample_length=365,
-                           engine='plotly') # Plot closing prices
 
-chart.update_layout(title=f'{cryptoHandler.symbol} Closing Prices', width=2400, height=1200)
-chart.show()
+
+
+
 
 
